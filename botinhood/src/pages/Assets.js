5d2a6431bf3069b1110b06f18e6473b31b88d9f2
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-03-06 09:11:30 -0700

    Taking from backend. and mapping.

--- botinhood/src/pages/Assets.js
+++ botinhood/src/pages/Assets.js
@@ -1,21 +1,48 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/Assets.css';
 import graph from '../assets/temp-graph.png'
 
 import StockRow from '../components/StockRow';
 
+const CLIENT_ID = '0b17ccc305d2be4a87d6d54135ea0f28';
+const REDIRECT_URI = 'http://localhost:3000/';
+
+// +-----------------Getting Stock Info-------------------+
+async function getStockData(botType){
+    let res = await fetch("http://localhost:8000/api/v1/stock/getLongShort", {
+        method: "GET",
+        headers: {
+            'Cache-Control': 'no-cache',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip,deflate,br'
+        }
+    })
+    console.log('waiting for response');
+    return await res.json();
+}
+
 
 const tempStockData = [
-    {id:1, name: 'apple', bought: '10', qauntity: 11},
-    {id:1, name: 'apple', bought: '10', qauntity: 11},
-    {id:1, name: 'apple', bought: '10', qauntity: 11},
-    {id:1, name: 'apple', bought: '10', qauntity: 11},
-    {id:1, name: 'apple', bought: '10', qauntity: 11},
-    {id:1, name: 'apple', bought: '10', qauntity: 11},
+    {props:{children:'None'}},
 ]
 
 function Assets(){
     const [myStocks, setMyStocks] = useState(tempStockData)
+    const [stockList, setStockList]= useState(tempStockData);
+
+    useEffect(() =>{
+        getStockData('LongShort').then((res) => {
+            
+            const stockListArray = []
+            
+            for (let i = 0; i < res.length; i++) {
+                const element = res[i];
+                stockListArray.push(<p className='stock_item'>{element}</p>)
+            }
+            console.log(stockListArray)
+            setStockList(stockListArray)
+        })
+    }, [])
 
     return (
         <div className='main_container'>
@@ -26,7 +53,7 @@ function Assets(){
 
                 <img id='graph' src={graph} alt="graph" />
 
-                {myStocks.map((post) =>
+                {stockList.map((post) =>
                     <StockRow content={post}/>
                 )}
                 <div>
