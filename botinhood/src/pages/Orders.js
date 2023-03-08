a1745c487a988df7443a6ac1dd56884d32124d0e
HaydonUresti <97619164+HaydonUresti@users.noreply.github.com>
2023-03-08 09:32:37 -0700

    Added async functions + styling to Orders

--- botinhood/src/pages/Orders.js
+++ botinhood/src/pages/Orders.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import '../styles/Orders.css';
 
+async function getShort(botType){
+    let res = await fetch("http://localhost:8000/api/v1/stock/getLong", {
+        method: "GET",
+        headers: {
+            'Cache-Control': 'no-cache',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip,deflate,br'
+        }
+    })}
+
+    async function getLong(botType){
+        let res = await fetch("http://localhost:8000/api/v1/stock/getShort", {
+            method: "GET",
+            headers: {
+                'Cache-Control': 'no-cache',
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip,deflate,br'
+            }
+        })}
+
 function Orders(){
     return (
         <div className="main_container">
-           <div id='order_container'>
+           <div className='order_container'>
             <p>Test line</p>
            </div>
         </div>
