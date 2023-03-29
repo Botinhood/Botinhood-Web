2288a3404b44f0728210bdb92093b31800fa613b
HaydonUresti <97619164+HaydonUresti@users.noreply.github.com>
2023-03-29 09:33:23 -0600

    Fixed quantity and what displays on Orders

--- botinhood/src/pages/Orders.js
+++ botinhood/src/pages/Orders.js
@@ -72,13 +72,24 @@ function Orders() {
 
     // Fetch Long trades and push each element into an array
     getLong('LongShort').then((res) => {
-      for (let i = 0; i < res.length; i++) {
-        const element = res[i];
-        stockListArray.push(<StockOrder name={element} />)
-      }
-      // Set the state of stockList with the array of Long trades
-      setStockList(stockListArray)
-    })
+        getQuantity().then((result) => {
+          let quantitiesList = Object.entries(result);
+  
+          // Map through the list of quantities and create a new StockOrder for each item
+          quantitiesList.map(function ([key, value]) {
+              console.log(res)
+              console.log("The key is ", key)
+              console.log(res.includes(key))
+              console.log(value)
+            if (res.includes(key)) {
+              stockListArray.push(<StockOrder name={key} quantity={value} />)
+                console.log("Pushed the value")
+            }
+            
+          })
+              // Set the state of stockList with the array of Long and Short trades
+              setStockList(stockListArray)
+          })})
 
     // Fetch Short trades and push each element into the same array as Long trades
     getShort('LongShort').then((res) => {
@@ -87,11 +98,15 @@ function Orders() {
 
         // Map through the list of quantities and create a new StockOrder for each item
         quantitiesList.map(function ([key, value]) {
+            
+            console.log(res)
+            console.log("The key is ", key)
+            console.log(res.includes(key))
+            console.log(value)
           if (res.includes(key)) {
             stockListArray.push(<StockOrder name={key} quantity={value} />)
-          } else {
-            stockListArray.push(<StockOrder name={key} quantity={'No quantity found'} />)
-          }
+        }
+    
         })
             // Set the state of stockList with the array of Long and Short trades
             setStockList(stockListArray)
@@ -99,7 +114,6 @@ function Orders() {
         // Set the state of stockList with the array of Long and Short trades
         setStockList(stockListArray)
     })}, [])
-// have activate only once 
     
 
     // Render the Orders page
